d873a932590e6e922fb72e79b03ceecf7c8ab972
mbergert <mtrujilloberger@gmail.com>
2019-08-24 13:06:08 -0400

    agrega modelo

--- TorneoVerificable/randomTorneo/models.py
+++ TorneoVerificable/randomTorneo/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Torneo(models.Model):
+    nombre = models.CharField(max_length=50)
+    max_equipos = models.IntegerField()
+    descr = models.CharField(max_length=300)
+    timestamp = models.BigIntegerField()
+    id_pulso = models.BigIntegerField()
+    listo = models.BooleanField()
+
+    def getEquipos(self):
+    	return Equipo.objects.get(id_torneo=self.id)
+
+    def getGrupo(self, idgrupo):
+    	grupo = Equipo.objects.get(id_torneo=self.id, id_grupo=idgrupo)
+    	return grupo
+
+    def countInscritos(self):
+    	return Equipo.objects.get(id_torneo=self.id).count()
+
+
+class Equipo(models.Model):
+	nombre = models.CharField(max_length=50)
+	id_torneo = models.ForeignKey(Torneo, on_delete=models.CASCADE)
+	id_grupo = models.IntegerField()
+
+
+    
+
+
+
